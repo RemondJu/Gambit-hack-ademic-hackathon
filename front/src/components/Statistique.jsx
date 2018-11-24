19e29c120ee8226b822e12df9131e1237d6c59b1
MorganCode <morgan.and.code@gmail.com>
2018-11-24 22:08:19 +0100

    t

--- front/src/components/Statistique.jsx
+++ front/src/components/Statistique.jsx
@@ -7,63 +7,62 @@ class Statistique extends Component {
   constructor(props){
     super(props)
     this.state = {
-      widthStatsEcology: '100px',
-      widthStatsCulture: '50px',
-      widthStatsHealth: '30px',
-      widthStatsSport: '20px',
-      widthStatsEducation: '70px',
-      widthStatsGastronomy: '35px',
-
+      widthStatsEcology: '100',
+      widthStatsCulture: '50',
+      widthStatsHealth: '30',
+      widthStatsSport: '20',
+      widthStatsEducation: '70',
+      widthStatsGastronomy: '35',
       widthStatsNumberEcology: '100',
       widthStatsNumberCulture: '50',
       widthStatsNumberHealth: '30',
       widthStatsNumberSport: '20',
       widthStatsNumberEducation: '70',
       widthStatsNumberGastronomy: '35',
-
-    }
-  }
+  
+    } }
+   
   render() {
     return (
       <div>
         <NavBarAccueil/>
         <NavBarNavigate/>
       <div className="containerStats">
-       <h2 style={{marginLeft:"50px"}}>Statistique</h2>
+       <h2 style={{marginLeft:"50px"}}>Statistique {this.state.currentCount}</h2>
        <div className="rowEcology">
        <img src="http://image.noelshack.com/fichiers/2018/47/6/1543050898-ecology.png" alt=""/>
-       <div style={{height: "15px", width: this.state.widthStatsEcology, backgroundColor:"blue", borderRadius:"10px"}}></div>
+       <div className="Ecology" style={{height: "15px", backgroundColor:"blue", borderRadius:"10px", width:this.state.widthStatsEcology + "px" }}></div>
        <p style={{marginLeft: "50px", fontWeight:"bold"}}>{this.state.widthStatsNumberEcology}%</p>
        </div>
 
        <div className="rowCulture">
        <img src="http://image.noelshack.com/fichiers/2018/47/6/1543050898-ticket.png" alt=""/>
-       <div style={{height: "15px", width: this.state.widthStatsCulture, backgroundColor:"blue", borderRadius:"10px"}}></div>
+       <div style={{height: "15px", width: this.state.widthStatsCulture + 'px', backgroundColor:"blue", borderRadius:"10px"}}></div>
        <p style={{marginLeft: "50px", fontWeight:"bold"}}>{this.state.widthStatsNumberCulture}%</p>
        </div>
 
        <div className="rowHealth">
        <img src="http://image.noelshack.com/fichiers/2018/47/6/1543050898-heart.png" alt=""/>
-       <div style={{height: "15px", width: this.state.widthStatsHealth, backgroundColor:"blue", borderRadius:"10px"}}></div>
+       <div style={{height: "15px", width: this.state.widthStatsHealth + 'px', backgroundColor:"blue", borderRadius:"10px"}}></div>
        <div className="progressStats"></div>
        <p style={{marginLeft: "50px",fontWeight:"bold"}}>{this.state.widthStatsNumberHealth}%</p>
        </div>
 
        <div className="rowSport">
        <img src="http://image.noelshack.com/fichiers/2018/47/6/1543050898-basketball.png" alt=""/>
-       <div style={{height: "15px", width: this.state.widthStatsSport, backgroundColor:"blue", borderRadius:"10px"}}></div>
+       <div style={{height: "15px", width: this.state.widthStatsSport + 'px', backgroundColor:"blue", borderRadius:"10px"}}></div>
        <p style={{marginLeft: "50px",fontWeight:"bold"}}>{this.state.widthStatsNumberSport}%</p>
        </div>
 
        <div className="rowEducation">
        <img   src="http://image.noelshack.com/fichiers/2018/47/6/1543050898-mortarboard.png" alt=""/>
-       <div style={{height: "15px", width: this.state.widthStatsEducation, backgroundColor:"blue", borderRadius:"10px"}}></div>
+       <div style={{height: "15px", width: this.state.widthStatsEducation + 'px', backgroundColor:"blue", borderRadius:"10px"}}></div>
        <p style={{marginLeft: "50px",fontWeight:"bold"}}>{this.state.widthStatsNumberEducation}%</p>
        </div>
 
        <div className="rowGastronomy">
        <img  src="http://image.noelshack.com/fichiers/2018/47/6/1543050898-curry.png" alt=""/>
-       <div style={{height: "15px", width: this.state.widthStatsGastronomy, backgroundColor:"blue", borderRadius:"10px"}}></div>
+       <div style={{height: "15px", width: this.state.widthStatsGastronomy + 'px', backgroundColor:"blue", borderRadius:"10px"}}></div>
        <p style={{marginLeft: "50px",fontWeight:"bold"}}>{this.state.widthStatsNumberGastronomy}%</p>
        </div>
 
