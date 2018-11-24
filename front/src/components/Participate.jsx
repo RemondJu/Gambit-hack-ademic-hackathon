1b2e4e8d56d3dc84d23851e249195551cc043e50
MorganCode <morgan.and.code@gmail.com>
2018-11-24 16:33:07 +0100

    final popup

--- front/src/components/Participate.jsx
+++ front/src/components/Participate.jsx
@@ -55,22 +55,6 @@ class Participate extends Component {
   render() {
     return (
       <div className='Participate'>
-<<<<<<< HEAD
-        <h2>Support the project :</h2>
-        <h3>{this.props.name}</h3>
-        <UserAccount />
-        <div className='Participation'>
-          <h3>I want to participate by :</h3>
-          <div class="slider-wrapper">
-            <span>Giving financial support</span><br/>
-            <input type='range' value={this.state.participationAmount} min='0' max='1000' step='10' onChange={this.handleParticipationamount}/>
-            <span>{this.state.participationAmount}</span>
-          </div>
-        </div>
-      {this.state.showCardTransaction ? 
-        <div className="transactionInformation">
-        <button onClick={() => this.handleClickTransactionInfoExit()}><img src="http://image.noelshack.com/fichiers/2018/47/6/1543062389-error-1.png" alt=""/></button>
-=======
       <NavBarNavigate/>
       <NavBarAccueil/>
       <h2>Support the project :</h2>
@@ -101,7 +85,6 @@ class Participate extends Component {
         </div>
         <div className="row"><p>Your payment of {this.state.participationAmount}€ <br/>for {this.props.name} was well taken into account.</p></div>
         <div className="row"><p>Thank you for your support !</p></div>
->>>>>>> popupbis
       </div> : null }
     </div>
      );
