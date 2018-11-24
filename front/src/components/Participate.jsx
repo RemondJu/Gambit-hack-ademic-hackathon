b38f4ed9962106281cd76f5aff65a438586f369e
MorganCode <morgan.and.code@gmail.com>
2018-11-24 16:29:06 +0100

    pop up fini

--- front/src/components/Participate.jsx
+++ front/src/components/Participate.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import UserAccount from './UserAccount';
 import '../css/Participate.css'
+import NavBarNavigate from './NavBarNavigate';
+import NavBarAccueil from './NavBarAccueil';
 
 
 class Participate extends Component {
@@ -8,7 +10,8 @@ class Participate extends Component {
     super(props);
     this.state = { 
       participationAmount: 0,
-      showCardTransaction: true,
+      showCardTransaction: false,
+      showCardTransactionValidate: false,
      }
 
      this.handleParticipationamount = this.handleParticipationamount.bind(this)
@@ -20,9 +23,11 @@ class Participate extends Component {
     })
   }
 
+
   handleClickTransactionValidate = () => {
     this.setState({ 
       showCardTransaction: true,
+      showCardTransactionValidate: false,
     })
   }
 
@@ -32,12 +37,26 @@ class Participate extends Component {
     })
   }
 
+  handleClickValidate = () => {
+    this.setState({ 
+      showCardTransactionValidate: true,
+    })
+  }
+
+  handleClickCancel = () => {
+    this.setState({ 
+      showCardTransactionValidate: false,
+    })
+  }
+
 
 
 
   render() {
     return (
       <div className='Participate'>
+      <NavBarNavigate/>
+      <NavBarAccueil/>
       <h2>Support the project :</h2>
       <h3>{this.props.name}</h3>
       <UserAccount />
@@ -49,12 +68,23 @@ class Participate extends Component {
         </div>
         <span>{this.state.participationAmount}</span>
       </div>
+      <button onClick={() => this.handleClickValidate()}>validate the transaction</button>
+      {this.state.showCardTransactionValidate ? 
+        <div className="transactionInformation">
+        <div className="row">
+        <button onClick={() => this.handleClickCancel()}><img src="http://image.noelshack.com/fichiers/2018/47/6/1543062389-error-1.png" alt=""/></button>
+        </div>
+        <div className="row"><p>You will finance the project: {this.props.name} of {this.state.participationAmount}€. Are you sure?</p></div>
+        <div><button onClick={() => this.handleClickTransactionValidate()}>Validate</button><button onClick={() => this.handleClickCancel()}>Cancel</button></div>
+      </div> : null }
+
       {this.state.showCardTransaction ? 
         <div className="transactionInformation">
-        
-        <button onClick={() => this.handleClickTransactionInfoExit()}><img src="http://image.noelshack.com/fichiers/2018/47/6/1543062389-error-1.png" alt=""/></button>
-        
-        
+        <div className="row">
+        <a href="/"><img src="http://image.noelshack.com/fichiers/2018/47/6/1543062389-error-1.png" alt=""/></a>
+        </div>
+        <div className="row"><p>Your payment of {this.state.participationAmount}€ <br/>for {this.props.name} was well taken into account.</p></div>
+        <div className="row"><p>Thank you for your support !</p></div>
       </div> : null }
     </div>
      );
