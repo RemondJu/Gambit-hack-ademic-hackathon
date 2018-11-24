eda8498b8250e8354f43f089f8ebd484e6cc7433
RemondJu <remond.neiluj@gmail.com>
2018-11-24 09:36:23 +0100

    flex on project card, route to project detail

--- front/src/components/ProjectCard.jsx
+++ front/src/components/ProjectCard.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { Button, Card, CardBody, CardSubtitle, CardText, CardTitle } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import '../css/ProjectCard.css'
 
 class ProjectCard extends Component {
   constructor(props) {
     super(props);
     this.state = {  }
   }
-  render() { 
-    return ( 
+  render() {
+    return (
       <div className='ProjectCard'>
-        
+        <Card>
+          <CardBody>
+            <CardTitle>{this.props.name}</CardTitle>
+            <CardSubtitle>{this.props.type}</CardSubtitle>
+          </CardBody>
+          <img width="100%" src={this.props.image} alt="project funding" />
+          <CardBody>
+            <CardText>{this.props.description.split(',').splice(0, 2).join('')+'.'}</CardText> 
+            <NavLink to={this.props.path}>
+              <Button>See more about it</Button>
+            </NavLink>
+          </CardBody>
+        </Card>
       </div>
      );
   }
