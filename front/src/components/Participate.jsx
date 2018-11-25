fd38fa645802dc7b805888fe2f7aadc5345dd4cf
RemondJu <remond.neiluj@gmail.com>
2018-11-25 10:28:06 +0100

    modifs

--- front/src/components/Participate.jsx
+++ front/src/components/Participate.jsx
@@ -55,38 +55,38 @@ class Participate extends Component {
   render() {
     return (
       <div className='Participate'>
-      <NavBarNavigate/>
-      <NavBarAccueil/>
-      <h2>Support the project :</h2>
-      <h3>{this.props.name}</h3>
-      <UserAccount />
-      <div className='Participation'>
-        <h3>I want to participate by :</h3>
-        <span>Giving financial support</span><br/>
-        <div class="slider-wrapper">
-        <input type='range' value={this.state.participationAmount} min='0' max='1000' step='10' onChange={this.handleParticipationamount}/>
+        <NavBarNavigate/>
+        <NavBarAccueil/>
+        <h2 className=''>Support the project :</h2>
+        <h3>{this.props.name}</h3>
+        <UserAccount />
+        <div className='Participation'>
+          <h3>I want to participate by :</h3>
+          <span>Giving financial support</span><br/>
+          <div class="slider-wrapper">
+            <input type='range' value={this.state.participationAmount} min='0' max='1000' step='10' onChange={this.handleParticipationamount}/>
+          </div>
+          <span>{this.state.participationAmount}</span>
         </div>
-        <span>{this.state.participationAmount}</span>
-      </div>
-      <button onClick={() => this.handleClickValidate()}>validate the transaction</button>
-      {this.state.showCardTransactionValidate ? 
+        <button onClick={() => this.handleClickValidate()}>validate the transaction</button>
+        {this.state.showCardTransactionValidate ? 
         <div className="transactionInformation">
-        <div className="row">
-        <button onClick={() => this.handleClickCancel()}><img src="http://image.noelshack.com/fichiers/2018/47/6/1543062389-error-1.png" alt=""/></button>
-        </div>
-        <div className="row"><p>You will finance the project: {this.props.name} of {this.state.participationAmount}€. Are you sure?</p></div>
-        <div><button onClick={() => this.handleClickTransactionValidate()}>Validate</button><button onClick={() => this.handleClickCancel()}>Cancel</button></div>
-      </div> : null }
+          <div className="row">
+            <button onClick={() => this.handleClickCancel()}><img src="http://image.noelshack.com/fichiers/2018/47/6/1543062389-error-1.png" alt=""/></button>
+          </div>
+          <div className="row"><p>You will finance the project: {this.props.name} of {this.state.participationAmount}€. Are you sure?</p></div>
+          <div><button onClick={() => this.handleClickTransactionValidate()}>Validate</button><button onClick={() => this.handleClickCancel()}>Cancel</button></div>
+        </div> : null }
 
-      {this.state.showCardTransaction ? 
-        <div className="transactionInformation">
-        <div className="row">
-        <a href="/"><img src="http://image.noelshack.com/fichiers/2018/47/6/1543062389-error-1.png" alt=""/></a>
-        </div>
-        <div className="row"><p>Your payment of {this.state.participationAmount}€ <br/>for {this.props.name} was well taken into account.</p></div>
-        <div className="row"><p>Thank you for your support !</p></div>
-      </div> : null }
-    </div>
+        {this.state.showCardTransaction ? 
+          <div className="transactionInformation">
+          <div className="row">
+          <a href="/"><img src="http://image.noelshack.com/fichiers/2018/47/6/1543062389-error-1.png" alt=""/></a>
+          </div>
+          <div className="row"><p>Your payment of {this.state.participationAmount}€ <br/>for {this.props.name} was well taken into account.</p></div>
+          <div className="row"><p>Thank you for your support !</p></div>
+        </div> : null }
+      </div>
      );
   }
 }
